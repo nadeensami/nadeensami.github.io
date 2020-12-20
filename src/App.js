fe675227f132e2f97196cb7bc0449ff77d5805c5
Nadeen <nadeen@ualberta.ca>
2020-12-20 09:35:30 -0700

    moved to using react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const App = () => (
+  <div class="landing-page">
+    <h1>beware: cool stuff coming soon to a personal site near you!</h1>
+    <h2>sometime this winterbreak, 2020</h2>
+    <p>sponsored by: perfectionism (tm) and the need to have a great website on the first go</p>
+    <button>
+      <a href="mailto:nadeen@ualberta.ca">let's connect!</a>
+    </button>
+  </div>
+);
+
+export default App;
